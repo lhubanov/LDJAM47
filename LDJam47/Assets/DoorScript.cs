b832e264078707c7fe74152209cd97e90148cded
archie_desktop <psibuck@gmail.com>
2020-10-03 20:56:31 +0100

    Added a door animation, that isn't quite working but it will get there with LachMagic

---
+++ LDJam47/Assets/DoorScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DoorScript : MonoBehaviour
+{
+    enum DoorState
+    {
+        CLOSED,
+        OPEN
+    }
+
+    Animator animator;
+    DoorState state = DoorState.CLOSED;
+
+    private void OnTriggerStay(Collider other)
+    {
+        animator.SetTrigger("Open");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if( state == DoorState.CLOSED)
+        {
+
+            animator.SetTrigger("Open");
+            state = DoorState.OPEN;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (state == DoorState.OPEN)
+        {
+            animator.SetTrigger("Close");
+            state = DoorState.CLOSED;
+        }
+    }
+}
