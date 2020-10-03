13ed8d6a7fc5a5543c87e6dd8aeff3c77a9f2be7
lhubanov <lachezar.hubanov@gmail.com>
2020-10-04 00:14:33 +0100

    some robo animation & reactions sctiprage

---
+++ LDJam47/Assets/Scripts/SpecialActionHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialActionHandler : MonoBehaviour
+{
+    // Emoji animator
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private SpriteRenderer reactionSpriteRenderer;
+
+    [SerializeField]
+    private Sprite[] reactionSprites;
+
+
+    // FIXME: LH:
+    // - Need to add collider
+    // - Need to set right sprite before animating
+    //      - Then just play the same sprite animation?
+    // - Can play other animations/two animations at same time?
+    // - Can play sounds
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        reactionSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter( Collider other )
+    {
+        SpecialActionEmitter specialActionEmitter = other.GetComponent<SpecialActionEmitter>();
+        if ( specialActionEmitter )
+        {
+            DoSpecialAction( specialActionEmitter.GetSpecialAction() );
+        }
+    }
+
+    private void DoSpecialAction( SPECIAL_ACTION action )
+    {
+        switch(action)
+        {
+            case SPECIAL_ACTION.HUM_MUSIC:
+                {
+                    // FIXME: LH: Messy way to do this? cause specialActions may not necessarily be sprite reactions
+                    // - whatever I'm tired
+                    reactionSpriteRenderer.sprite = reactionSprites[ (int)SPECIAL_ACTION.HUM_MUSIC ];
+
+                    animator.Play( Animator.StringToHash("SpriteReactionAnimation") );
+                    break;
+                }
+            case SPECIAL_ACTION.SMILE:
+                {
+                    reactionSpriteRenderer.sprite = reactionSprites[(int)SPECIAL_ACTION.SMILE];
+
+
+                    animator.Play( Animator.StringToHash("SpriteReactionAnimation") );
+                    break;
+                }
+        }
+    }
+}
