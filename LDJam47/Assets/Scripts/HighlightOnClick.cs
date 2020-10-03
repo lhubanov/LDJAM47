9953ccc980ba23effc0fcc6f02f32037c81cd286
lhubanov <lachezar.hubanov@gmail.com>
2020-10-03 18:28:54 +0100

    hover highlighting shader

---
+++ LDJam47/Assets/Scripts/HighlightOnClick.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightOnClick : MonoBehaviour
+{
+    [SerializeField]
+    private Material material;
+
+    void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        if( renderer )
+        {
+            material = renderer.material;
+        }
+    }
+
+    void OnMouseOver()
+    {
+        // FIXME: LH: set to green
+        material.SetColor("_OutlineColor", new Color(0f, 1f, 0.39f, 1f));
+        material.SetFloat("_Outline", 0.1f);
+    }
+
+    private void OnMouseExit()
+    {
+        material.SetColor("_OutlineColor", new Color(0f, 0f, 0f, 1f));
+        material.SetFloat("_Outline", 0f);
+    }
+}
