e458c31eb37f3dbcb8b89b9451928a1fe8e5f13b
Petros Bantolas <petermer@gmail.com>
2020-10-03 18:45:20 +0100

    Work WIP for explosions

---
+++ LDJam47/Assets/ExplosionInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class ExplosionInteraction : SphericalInteractionBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay( Collider other )
+    {
+        if ( other.attachedRigidbody )
+        {
+            
+			other.attachedRigidbody.AddExplosionForce( 100.0f, transform.position, GetComponent<SphereCollider>().radius );
+		}
+    }
+}
