523881d7aeed1f517a472b0c42ee243d0bf59ede
isaacsimmons96 <isaacsimmons96@yahoo.co.uk>
2020-10-04 18:35:33 +0100

    Added Sliding door with script to make it open Some other shit

---
+++ LDJam47/Assets/door_opener.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door_opener : MonoBehaviour
+{
+    public GameObject left_door;
+    public GameObject right_door;
+    public Animator door_animator;
+
+    bool test_bool = false;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if(!test_bool)
+            {
+                open_doors();
+                test_bool = true;
+            }
+            else
+            {
+                close_doors();
+                test_bool = false;
+            }
+          
+        }
+    }
+
+    public void open_doors()
+    {
+        door_animator.SetBool("Open", true);
+    }
+
+    public void close_doors()
+    {
+        door_animator.SetBool("Open", false);
+    }
+}
