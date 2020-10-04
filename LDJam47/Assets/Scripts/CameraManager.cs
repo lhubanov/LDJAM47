e2481d4525ed1d1c89501b9bd977f85c3fc9c684
archie_desktop <psibuck@gmail.com>
2020-10-04 21:53:19 +0100

    Added Camera functionality

---
+++ LDJam47/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera MainCamera;
+    public List<GameObject> CameraPositions;
+    public List<GameObject> CameraTargets;
+    private int CamPosNum = -1;
+
+    void Start()
+    {
+        MainCamera = Camera.main;
+        SelectNextCamera();
+    }
+
+    void SelectNextCamera()
+    {
+        CamPosNum = CamPosNum + 1;
+        MainCamera.transform.position = CameraPositions[CamPosNum].transform.position;
+        MainCamera.transform.LookAt(CameraTargets[CamPosNum].transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SelectNextCamera();
+        }
+    }
+}
