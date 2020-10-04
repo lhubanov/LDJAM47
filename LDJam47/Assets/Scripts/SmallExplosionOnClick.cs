2c7f928556afea03a2598126765495161a8e7831
lhubanov <lachezar.hubanov@gmail.com>
2020-10-04 13:38:57 +0100

    explosion on click script (for panel falling off and other things)

---
+++ LDJam47/Assets/Scripts/SmallExplosionOnClick.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallExplosionOnClick : MonoBehaviour
+{
+    [SerializeField]
+    private float explosionForce = 10.0f;
+
+    [SerializeField]
+    private float explosionRadius = 10.0f;
+
+    [SerializeField]
+    private float explosionUpwardsModifier = 10.0f;
+
+
+    [SerializeField]
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseDown()
+    {
+        if(rigidbody)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if ( Physics.Raycast(ray, out hitInfo) )
+            {
+                rigidbody.AddExplosionForce(explosionForce, hitInfo.point, explosionRadius, explosionUpwardsModifier);
+            }
+        }
+    }
+}
