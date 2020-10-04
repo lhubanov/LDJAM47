7b71f31122c54a200f9aea80784b155e1a02f375
archie_desktop <psibuck@gmail.com>
2020-10-04 22:45:30 +0100

    Updated the slidings doors to automatically open when the roboto walks to them and imported our first door into the main scene

--- LDJam47/Assets/door_opener.cs
+++ LDJam47/Assets/door_opener.cs
@@ -4,28 +4,16 @@ using UnityEngine;
 
 public class door_opener : MonoBehaviour
 {
-    public GameObject left_door;
-    public GameObject right_door;
     public Animator door_animator;
 
-    bool test_bool = false;
-
-    public void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        if (Input.GetKeyDown("space"))
-        {
-            if(!test_bool)
-            {
-                open_doors();
-                test_bool = true;
-            }
-            else
-            {
-                close_doors();
-                test_bool = false;
-            }
-          
-        }
+        open_doors();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        close_doors();
     }
 
     public void open_doors()
