9058bc9446c65f14c00f09575081039ea677fa28
archie_desktop <psibuck@gmail.com>
2020-10-05 19:06:47 +0100

    Added a wait time once the robot is done and also a rotation at the end

--- LDJam47/Assets/RobotAnimationController.cs
+++ LDJam47/Assets/RobotAnimationController.cs
@@ -7,7 +7,7 @@ public class RobotAnimationController : MonoBehaviour
     public Animator robot_animator;
     public Animator robot_wheel_animator;
     Vector3 previous_position;
-
+    
     // Start is called before the first frame update
     void Start()
     {
