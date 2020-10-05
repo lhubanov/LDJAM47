debd958e425bd8177694b00783cc574618413441
isaacsimmons96 <isaacsimmons96@yahoo.co.uk>
2020-10-05 13:27:20 +0100

    Added animations to the Robot

---
+++ LDJam47/Assets/RobotAnimationController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotAnimationController : MonoBehaviour
+{
+    public Animator robot_animator;
+    public Animator robot_wheel_animator;
+    Vector3 previous_position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previous_position = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        float speed = Vector3.Distance(transform.position, previous_position);
+        previous_position = transform.position;
+
+        if ( speed > 0.1f)
+        {
+            robot_animator.SetBool("Moving", true);
+            robot_wheel_animator.SetBool("Moving", true);
+        }
+        else
+        {
+            robot_animator.SetBool("Moving", false);
+            robot_wheel_animator.SetBool("Moving", false);
+        }
+    }
+
+}
